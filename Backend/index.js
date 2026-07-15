a44a9f6a62273b16fd7d02d27dce27fde649706b
Shubham Bhardwaj <shubhambhardwaj10oct@gmail.com>
2026-07-16 01:28:58 +0530

    fix env variable name for CORS

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const app=express()
 
 // frontend + Backend connection
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
 }))
 
